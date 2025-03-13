a24070a872ce273ee6a717f58fc4b5dc192cb3fb
hanhercales <deathchan505@gmail.com>
2025-03-14 00:54:44 +0700

    a

--- Build/Test.framework.js
+++ Build/Test.framework.js
@@ -1993,13 +1993,13 @@ var tempI64;
 // === Body ===
 
 var ASM_CONSTS = {
-  3857312: function() {return Module.webglContextAttributes.premultipliedAlpha;},  
- 3857373: function() {return Module.webglContextAttributes.preserveDrawingBuffer;},  
- 3857437: function() {return Module.webglContextAttributes.powerPreference;},  
- 3857495: function() {Module['emscripten_get_now_backup'] = performance.now;},  
- 3857550: function($0) {performance.now = function() { return $0; };},  
- 3857598: function($0) {performance.now = function() { return $0; };},  
- 3857646: function() {performance.now = Module['emscripten_get_now_backup'];}
+  3857328: function() {return Module.webglContextAttributes.premultipliedAlpha;},  
+ 3857389: function() {return Module.webglContextAttributes.preserveDrawingBuffer;},  
+ 3857453: function() {return Module.webglContextAttributes.powerPreference;},  
+ 3857511: function() {Module['emscripten_get_now_backup'] = performance.now;},  
+ 3857566: function($0) {performance.now = function() { return $0; };},  
+ 3857614: function($0) {performance.now = function() { return $0; };},  
+ 3857662: function() {performance.now = Module['emscripten_get_now_backup'];}
 };
 
 
